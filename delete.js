87248bbf694d99602a295673436fc77be4fa29f0
Bruno Tavares <35853042+brunobtavares@users.noreply.github.com>
2019-10-09 09:48:34 -0300

    atualizado.

--- delete.js
+++ delete.js
@@ -1,3 +1,8 @@
+/*
+* @author => Bruno Tavares
+* @link   => github.com/brunobtavares/deleteFriendsFacebook
+*/
+
 /*! jQuery v3.4.1 | (c) JS Foundation and other contributors | jquery.org/license */ ! function(e, t) {
   "use strict";
   "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
